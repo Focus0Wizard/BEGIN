ca5a9bbef7d95cbfe2a8f557ad415269106eac50
Focus0Wizard <jhonn.ramirez@ucb.edu.bo>
2024-11-20 16:28:06 -0400

    se añadio varios links de para moverse dentro la interfaz

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductosComponent } from './paginas/productos/productos.component';
+import { ContactosComponent } from './paginas/contactos/contactos.component';
+import { HomeComponent } from './paginas/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'productos', component: ProductosComponent},
+    {path: 'contacto', component: ContactosComponent},
+    {path: 'home', component: HomeComponent}
+];
